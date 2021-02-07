559ed600423286e758daa51cd35ab6bf1daab923
rasputinforever <erik.justin.portillo@gmail.com>
2021-02-06 20:41:02 -0800

    psuedo code for plan of attack and min. requirements

---
+++ assetts/employeeManager.sql
@@ -0,0 +1,24 @@
+-- this is a 'starter' to get a database going for the user
+-- include some optional 'starter' data for demonstration purposes
+
+-- Tables We need....
+
+-- department
+    -- id
+    -- name
+
+-- role
+    -- id
+    -- title
+    -- salary 
+    -- department_id (connection)
+
+-- employee
+    -- id
+    -- first_name
+    -- last name
+    -- role_id (conn)
+    -- manager_id (emp id of that manager)
+
+-- of note is the fact that managers and employees are stored on the same table. Not exactly a big deal.
+
