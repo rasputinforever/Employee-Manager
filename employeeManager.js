272833f1884e5edd77ed3b991ab3c573a3f0843e
rasputinforever <erik.justin.portillo@gmail.com>
2021-02-06 20:41:29 -0800

    removes express.js, not needed

---
+++ employeeManager.js
@@ -0,0 +1,20 @@
+// employee manager
+// I installed express... but I don't think we actually needed it.
+// INQUIRER is the user interface.
+
+// initiate code
+    // mandatory tools:
+        // add departments, roles, and employees
+            // there's a table for each of these
+        // look at departments, roles, and employees -- think about what would be a practical way to 'see' this info
+        // update/edite employee roles
+
+    // "extras" suggested to add if possible
+        // edit who an employee's manager is
+        // view all employees under a manager
+        // delete any of the above
+            // deleting a manager creates an odd situation where you need to re-assign each employee to a new manager
+            // same with departments!
+        // combined salaray for a single department
+        
+// first thing's first... create the db! Can't quite do much without working data.
