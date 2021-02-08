d254b25bf51986659dd08b31389f854c96abd459
rasputinforever <erik.justin.portillo@gmail.com>
2021-02-07 23:35:57 -0800

    createTitle works

--- employeeManager.js
+++ employeeManager.js
@@ -86,7 +86,35 @@ initEmpMan();
 function createTitl() {    
     console.log("Coming Soon!")
     // titles require a department, so get a list of departments then PROMPT for the rest...
+    getDepartments().then((depList) => {
+        
+        // create array of departments
+        let departments = [];
+        depList.forEach(dep => {
+            departments.push(dep.department);
+        });
 
+        // inquire here: dep, salary, title
+        inquirer.prompt([{
+            name: 'title',
+            type: 'input',
+            message: 'What is the name of this new TITLE?'
+        },{
+            name: 'salary',
+            type: 'input',
+            message: 'What is the SALARY for this new title?'
+        },{
+            name: 'department',
+            type: 'list',
+            message: 'What DEPARTMENT will this title belong to?',
+            choices: departments
+        }]).then((res) => {
+            // get depID
+            const foundDep = depList.find(dep => dep.department === res.department);
+            // send
+            addTitle(res.title, res.salary, foundDep.id)
+        })
+    })
 }
 
 // works, move to lib modules when complete
