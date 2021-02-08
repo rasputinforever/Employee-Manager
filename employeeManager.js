e1bceda145160a4154ca15e5a2bc0cd16e1ebb58
rasputinforever <erik.justin.portillo@gmail.com>
2021-02-07 20:55:59 -0800

    moves summary scripts to module

--- employeeManager.js
+++ employeeManager.js
@@ -7,6 +7,7 @@
         // add departments, roles, and employees
             // there's a table for each of these
         // look at departments, roles, and employees -- think about what would be a practical way to 'see' this info
+                // check on all these. Might improve later.
         // update/edite employee roles
 
     // "extras" suggested to add if possible
@@ -21,10 +22,11 @@
 
 // npms
 const inquirer = require('inquirer');
-const cTable = require('console.table');
 
 // modules
-const getAllQuery = require('./lib/mysqlQuery.js')
+const empSummary = require('./lib/summarizeData.js')
+const depSummary = require('./lib/summarizeData.js')
+const titlSummary = require('./lib/summarizeData.js')
 
 // starts here
 function initEmpMan() {
@@ -38,13 +40,13 @@ function initEmpMan() {
         // get the read info here. Used for ALL subsequent functions!
         switch(res.function) {
             case 'Employee Summaries':        
-                empSummary(res)
+                empSummary()
                 break;
             case 'Department Summaries':
-                empSummary();
+                depSummary();
                 break;
             case 'Summary of Job Titles':
-                titleSUmmary();
+                titlSummary();
                 break;
             default:
                 console.log("coming soon!")
@@ -54,104 +56,7 @@ function initEmpMan() {
     })
 }
 
+// start!
 initEmpMan();
 
-function empSummary() {
-    getAllQuery().then((data) => {
-        console.log("Employee Summary...")
-        console.table(data);
-    })
-}
-
-function empSummary() {
-    getAllQuery().then((data) => {
-        // get a list of distinct deps using the same query
-        let depList = [];
-        let dupeCheck;
-        data.forEach(employee => {
-            dupeCheck = depList.indexOf(employee.department) === -1;            
-            if (dupeCheck) {
-                depList = [...depList, employee.department];
-            }
-        });
-
-        // inquire which summary we need
-        inquirer.prompt({
-            name: 'department',
-            type: 'list',
-            choices: depList,
-            message: 'Which department would you like a summary of?'
-        }).then((res) => {
-            console.log('Summary for ', res.department)
-            // ok now do a summary! haha! Re-organize desired data into an object which will be displayed
-            let depObj = {
-                Department: res.department,
-                Managers: [],
-                Employees: [],
-                Total_Salary: 0.00
-            }
-
-            data.forEach(employee => {
-                if (employee.department === depObj.Department) {
-                    depObj.Employees = [...depObj.Employees, employee.name];
-                    depObj.Total_Salary += employee.salary; 
-                    // manager check
-                    if (employee.title.includes('Manager')) {
-                        depObj.Managers.push(employee.name);
-                    } 
-                } 
-            });
-
-            depObj.Total_Salary = `$${depObj.Total_Salary}`
-            // display final table of info
-            console.table(depObj);
-        })
-    })
-}
-
-function titleSUmmary() {
-    getAllQuery().then((data) => {
-        // get a list of distinct deps using the same query
-        let titleList = [];
-        let dupeCheck;
-        data.forEach(employee => {
-            dupeCheck = titleList.indexOf(employee.title) === -1;            
-            if (dupeCheck) {
-                titleList = [...titleList, employee.title];
-            }
-        });
-
-        // inquire which summary we need
-        inquirer.prompt({
-            name: 'title',
-            type: 'list',
-            choices: titleList,
-            message: 'Which title would you like a summary of?'
-        }).then((res) => {
-            console.log('Summary for ', res.title)
-            // ok now do a summary! haha! Re-organize desired data into an object which will be displayed
-            let titlObj = {
-                Title: res.title,
-                Employees: [],
-                Managers: '',
-                Salary: 0.00
-            }
-            // get a list of all employees currently with that title.
-            // the assumption made here is that the salary for each person with that title is the same.
-            data.forEach(employee => {
-                if (employee.title === titlObj.Title) {
-                    titlObj.Employees = [...titlObj.Employees, employee.name];
-                    titlObj.Salary = employee.salary; 
-                    titlObj.Managers = employee.managerID
-                } 
-            });
-            // get manager(s)
-
-            
-            titlObj.Salary = `$${titlObj.Salary}`
-            // display final table of info
-            console.table(titlObj);
-        })
-    })
-}
 
